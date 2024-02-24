f3983edc6f559a2f0a4d62828362fc9d8ea394ac
madhuravani7 <madhubudige67@gmail.com>
2024-02-25 04:51:17 +1100

    index.html finished

---
+++ index.js
@@ -0,0 +1,11 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 won";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 won🚩";
+}else {
+    document.querySelector("h1").innerHTML="Draw";
+}
